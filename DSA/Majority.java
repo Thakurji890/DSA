b74216008501f8ff318d3299f18b6776d670eab6
Thakurji890 <tha.rupesh456@gmail.com>
2026-04-15 21:09:47 +0530

    more questions

--- DSA/Majority.java
+++ DSA/Majority.java
@@ -5,8 +5,11 @@ import java.util.HashSet;
 
 public class Majority {
     public static void main(String[] args) {
-        majorityElement();
-        unionOfArray();
+        // majorityElement();
+        // unionOfArray();
+        // intersectionOfArray();
+        itineraryTickets();
+        subArraySum();
     }
 
     // Question 1 : Majority Element
@@ -59,5 +62,79 @@ public class Majority {
     public static void intersectionOfArray() {
         int[] arr1 = { 7, 3, 9 };
         int[] arr2 = { 6, 3, 9, 2, 9, 4 };
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++)
+            set.add(arr1[i]);
+        int count = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            if (set.contains(arr2[i])) {
+                count++;
+                System.out.print(arr2[i] + " ");
+                set.remove(arr1[i]);
+            }
+        }
+        System.out.println("\nNo of Common Element in both array : " + count);
+    }
+
+    // Find itinerary from tickets
+    // chennai -> bengaluru
+    // mumbai -> delhi
+    // goa -> chennai
+    // delhi -> goa
+
+    public static void itineraryTickets() {
+        HashMap<String, String> route = new HashMap<>();
+        route.put("Chennai", "Bengluru");
+        route.put("Mumbai", "Delhi");
+        route.put("Goa", "Chennai");
+        route.put("Delhi", "Goa");
+
+        String strPoint = startingPoint(route);
+
+        while (route.containsKey(strPoint)) {
+            System.out.print(strPoint + " => ");
+            strPoint = route.get(strPoint);
+        }
+        System.out.println(strPoint);
+    }
+
+    public static String startingPoint(HashMap<String, String> route) {
+        HashMap<String, String> revRoute = new HashMap<>();
+
+        for (String key : route.keySet()) {
+            revRoute.put(route.get(key), key);
+        }
+
+        for (String key : route.keySet()) {
+            if (!revRoute.containsKey(key))
+                return key;
+        }
+        return null;
+    }
+
+    // Subarray sum equal to k
+
+    public static void subArraySum() {
+        int[] arr = { 10, 2, -2, -20, 10 };
+        int k = -10;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int ans = 0;
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (map.containsKey(sum - k))
+                ans += map.get(sum - k);
+            if (map.containsKey(sum))
+                map.put(sum, map.get(sum) + 1);
+            else
+                map.put(sum, 1);
+        }
+        System.out.println(ans);
+
     }
 }
