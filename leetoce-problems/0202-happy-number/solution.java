12c6b880db012a9da2ad60184012fbcd3e9a1a0e
Thakurji890 <tha.rupesh456@gmail.com>
2026-04-19 05:31:37 +0000

    [LeetCode Sync] Runtime - 1 ms (77.84%), Memory - 42.2 MB (79.76%)

---
+++ leetoce-problems/0202-happy-number/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = square(n);
+        int fast = square(square(n));
+
+        while(slow != fast) {
+            slow = square(slow);
+            fast = square(square(fast));
+            if(slow == 1 || fast == 1)
+                return true;
+        }
+        return slow == 1;
+    }
+    public static int square(int n) {
+        int ans = 0;
+        while(n > 0) {
+            int d = n % 10;
+            ans += d * d;
+            n /= 10;
+        }
+        return ans;
+    }
+}
