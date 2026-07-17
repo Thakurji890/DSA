03264a92de95e451b0e97faf7515e1b8973e5c2d
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-17 19:34:34 +0530

    optimal approach added in countinversions

--- campx/CountInversions.java
+++ campx/CountInversions.java
@@ -20,10 +20,65 @@ public class CountInversions {
             }
         }
         System.out.println(count);
+
+        System.out.println(countInversions(arr));
     }
 
-    public static void betterSolution(int[] arr) {
+    // Optimal Solution
 
+    public static long mergeSortAndCount(int[] arr, int[] temp, int left, int right) {
+        long count = 0;
+
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            count += mergeSortAndCount(arr, temp, left, mid);
+
+            count += mergeSortAndCount(arr, temp, mid + 1, right);
+
+            count += mergeAndCount(arr, temp, left, mid, right);
+        }
+
+        return count;
+    }
+
+    public static long mergeAndCount(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        long count = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                count += (mid - i + 1);
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
+
+        return count;
+    }
+
+    public static long countInversions(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+
+        int[] temp = new int[arr.length];
+        return mergeSortAndCount(arr, temp, 0, arr.length - 1);
     }
 
 }
