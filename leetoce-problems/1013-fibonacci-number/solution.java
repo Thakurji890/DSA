cb84ac4842002ebf3fd52392324e930d38406b82
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-19 13:52:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (52.96%)

---
+++ leetoce-problems/1013-fibonacci-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if (n < 2)
+            return n;
+        int prev2 = 0;
+        int prev = 1;
+        for (int i = 2; i <= n; i++) {
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
