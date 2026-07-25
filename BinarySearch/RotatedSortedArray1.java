562f3c21ee93360590eb81bc83c8ca29002a020c
Thakurji890 <tha.rupesh456@gmail.com>
2026-07-25 20:33:51 +0530

    array rotation2 added

--- BinarySearch/RotatedSortedArray1.java
+++ BinarySearch/RotatedSortedArray1.java
@@ -2,9 +2,10 @@ package BinarySearch;
 
 public class RotatedSortedArray1 {
     public static void main(String[] args) {
-        int[] arr = { 7, 8, 9, 1, 2, 3, 4, 5, 6 };
+        int[] arr = { 7, 8, 9, 1, 1, 2, 3, 4, 5, 6 };
         int target = 1;
         System.out.println(solution(arr, target));
+        System.out.println(solutionDuplicates(arr, target));
     }
 
     public static int solution(int[] arr, int target) {
@@ -32,6 +33,38 @@ public class RotatedSortedArray1 {
             }
         }
         return -1;
+    }
+
+    public static boolean solutionDuplicates(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return true;
+            if (arr[left] == arr[mid] && arr[right] == arr[mid]) {
+                left += 1;
+                right -= 1;
+                continue;
+            }
+
+            if (arr[left] <= arr[mid]) {
+
+                if (arr[left] <= target && target < arr[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+
+            } else {
+                if (arr[mid] < target && target < arr[right])
+                    right = mid + 1;
+                else
+                    left = mid - 1;
+            }
+        }
+        return false;
     }
 
 }
