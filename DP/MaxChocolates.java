abea8ab491e91ca37c569269a2260fd758da2d13
Thakurji890 <tha.rupesh456@gmail.com>
2026-08-07 15:32:23 +0530

    Add MaxChocolates solution - DP

---
+++ DP/MaxChocolates.java
@@ -0,0 +1,14 @@
+package DP;
+
+public class MaxChocolates {
+    public static void main(String[] args) {
+
+    }
+
+    // public static int recurstion(int[][] arr, int i1, int j1, int i2, int j2) {
+    // if (j1 < 0 || j1 >= arr.length || j2 < 0 || j2 > arr.length) {
+    // return (int) -1e9;
+    // }
+    // }
+
+}
