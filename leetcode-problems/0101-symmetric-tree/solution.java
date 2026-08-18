ceae810751dfcddf61c03383f891d075577d73de
Thakurji890 <tha.rupesh456@gmail.com>
2026-08-18 05:18:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (86.31%)

---
+++ leetcode-problems/0101-symmetric-tree/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+        if (leftNode == null || rightNode == null) {
+            return false;
+        }
+
+        if (leftNode.val != rightNode.val) {
+            return false;
+        }
+
+        return helper(leftNode.left, rightNode.right) && helper(leftNode.right, rightNode.left);
+    }
+}
